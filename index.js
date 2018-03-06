fb03db8ed8346879ddb1e1118b724981aaecb5f8
ana.rajner@gmail.com <ana.rajner@gmail.com>
2018-03-06 14:42:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday,name) {
   return (`Happy ${holiday}, ${name}!`)
 }
 function holidayCountdown(holiday,days) {
-  return (`It\ns ${days} days until ${holiday}!`)
+  return (`It´s ${days} days until ${holiday}!`)
 }
